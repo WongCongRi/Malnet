dcdb1c1852c4def3d0928e81d61ea72b96655f8c
Jonathan <jonathantay13@yahoo.com.sg>
2018-10-09 18:15:12 +0800

    091018

---
+++ about.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+
+<?php 
+  require_once(__DIR__."/src/config.php");
+  require_once(__DIR__."/src/lib/usercake/init.php");
+  if (!UCUser::CanUserAccessUrl($_SERVER['PHP_SELF'])){die();}
+  $user = UCUser::getCurrentUser();
+?> 
+
+<?php 
+  $filters = array();
+  if (isset($_GET["tag"])) {
+  	$filters["tag"] = $_GET["tag"];
+  }
+?>
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?php echo $user_settings->WebsiteName() ?></title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="plugins/ionicons/css/ionicons.min.css">
+  <link href="http://localhost/mrf/dist/img/Malnet_logo.jpg" rel="shortcut icon" type="image/x-icon" />
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.css">
+  <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+        page. However, you can choose any other skin. Make sure you
+        apply the skin class to the body tag so the changes take effect.
+  -->
+  <link rel="stylesheet" href="dist/css/skins/skin-green.min.css">
+
+  <!-- Generic page styles -->
+  <link rel="stylesheet" href="plugins/jQueryUpload/css/style.css">
+  <!-- blueimp Gallery styles -->
+  <link rel="stylesheet" href="plugins/jQueryUpload/css/blueimp-gallery.min.css">
+  <!-- CSS to style the file input field as button and adjust the Bootstrap progress bars -->
+  <link rel="stylesheet" href="plugins/jQueryUpload/css/jquery.fileupload.css">
+  <link rel="stylesheet" href="plugins/jQueryUpload/css/jquery.fileupload-ui.css">
+  <!-- CSS adjustments for browsers with JavaScript disabled -->
+  <noscript><link rel="stylesheet" href="plugins/jQueryUpload/css/jquery.fileupload-noscript.css"></noscript>
+  <noscript><link rel="stylesheet" href="plugins/jQueryUpload/css/jquery.fileupload-ui-noscript.css"></noscript>
+  <!-- jqPagination styles -->
+  <link rel="stylesheet" href="plugins/jQueryUpload/css/jqpagination.css" />	
+  <!-- tags -->
+  <link rel="stylesheet" type="text/css" href="plugins/jQueryUpload/css/tagmanager.css" />
+  <!-- Pace style -->
+  <link rel="stylesheet" href="plugins/pace/pace.min.css">
+  
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <style type="text/css">
+    .table-responsive {
+		min-height: 400px !important;
+	}
+	
+	ul#dropdown-item-actions,
+	ul#dropdown-item-actions {
+	    z-index: 10000;
+	}
+  </style>
+  
+</head>
+<body class="hold-transition skin-green sidebar-mini">
+<div class="wrapper">
+
+  <?php  include(__DIR__."/top-nav.php"); ?> 
+  <?php  include(__DIR__."/left-nav.php"); ?> 
+  
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+
+    <!-- Main content -->
+    <section class="content">
+
+	<!-- Your Page Content Here -->
+	<div class="panel panel-info">
+		<div class="panel-heading">Repository Information</div>
+</body>
+</html>
--- left-nav.php
+++ left-nav.php
@@ -79,7 +79,7 @@
 		</li>
 		<?php } ?>
 	<?php } ?>	
-	<!--<li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>-->	
+	<li><a href="#"><i class="fa fa-link"></i> <span>About</span></a></li>
 	<!--<li class="treeview">
 		<a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
 		<span class="pull-right-container">
@@ -90,8 +90,8 @@
 		<li><a href="#">Link in level 2</a></li>
 		<li><a href="#">Link in level 2</a></li>
 		</ul>
-	</li>-->
-	</ul>
+	</li>
+	</ul>-->
 	<!-- /.sidebar-menu -->
 </section>
 <!-- /.sidebar -->
